6835fdf0378464502226f682095dae818f3b70e8
Diego Ferreira <dlins_ferreira@hotmail.com>
2025-03-04 10:28:45 -0500

    The criation of the first migration

---
+++ ReservationApp.DAL/Migrations/20250304151821_InitialCreate.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurant",
+                columns: table => new
+                {
+                    RestaurantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "varchar(1000)", unicode: false, maxLength: 1000, nullable: true),
+                    Address = table.Column<string>(type: "varchar(300)", unicode: false, maxLength: 300, nullable: true),
+                    PhoneNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Restaura__87454C95A8FACA11", x => x.RestaurantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "varchar(320)", unicode: false, maxLength: 320, nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(64)", maxLength: 64, nullable: true),
+                    Birthdate = table.Column<DateOnly>(type: "date", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Users__1788CC4C4E85B694", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RestaurantHolidays",
+                columns: table => new
+                {
+                    HolidayId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RestaurantId = table.Column<int>(type: "int", nullable: false),
+                    HolidayDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Restaura__2D35D57A8773257F", x => x.HolidayId);
+                    table.ForeignKey(
+                        name: "FK__Restauran__Resta__30F848ED",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurant",
+                        principalColumn: "RestaurantId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RestaurantOpenDays",
+                columns: table => new
+                {
+                    OpenDayId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RestaurantId = table.Column<int>(type: "int", nullable: false),
+                    WeekDay = table.Column<byte>(type: "tinyint", nullable: false),
+                    OpeningTime = table.Column<TimeOnly>(type: "time", nullable: false),
+                    ClosingTime = table.Column<TimeOnly>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Restaura__64C7ED10E86A0350", x => x.OpenDayId);
+                    table.ForeignKey(
+                        name: "FK__Restauran__Resta__2E1BDC42",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurant",
+                        principalColumn: "RestaurantId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservation",
+                columns: table => new
+                {
+                    ReservationId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    RestaurantId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "varchar(320)", unicode: false, maxLength: 320, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    ReservationDate = table.Column<DateTime>(type: "smalldatetime", nullable: false),
+                    PeopleCount = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<byte>(type: "tinyint", nullable: true, defaultValue: (byte)0),
+                    HiddenFromUser = table.Column<bool>(type: "bit", nullable: true, defaultValue: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Reservat__B7EE5F24B9EDEE04", x => x.ReservationId);
+                    table.ForeignKey(
+                        name: "FK__Reservati__Resta__4E88ABD4",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurant",
+                        principalColumn: "RestaurantId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK__Reservati__UserI__4D94879B",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAuthProvider",
+                columns: table => new
+                {
+                    AuthId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    AuthProvider = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__UserAuth__12C15DD3DC9BF2AF", x => x.AuthId);
+                    table.ForeignKey(
+                        name: "FK__UserAuthP__UserI__29572725",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_RestaurantId",
+                table: "Reservation",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_UserId",
+                table: "Reservation",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantHolidays_RestaurantId",
+                table: "RestaurantHolidays",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantOpenDays_RestaurantId",
+                table: "RestaurantOpenDays",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAuthProvider_UserId",
+                table: "UserAuthProvider",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__Users__A9D10534E20A3016",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservation");
+
+            migrationBuilder.DropTable(
+                name: "RestaurantHolidays");
+
+            migrationBuilder.DropTable(
+                name: "RestaurantOpenDays");
+
+            migrationBuilder.DropTable(
+                name: "UserAuthProvider");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
